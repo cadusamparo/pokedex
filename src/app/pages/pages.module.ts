555f2356a70e6ef5e59a771e7a2609b35cc14547
Carlos Eduardo Braz Samparo <carlossamparoo@gmail.com>
2024-03-05 14:20:42 -0300

    first commit

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './rounting.module';
+import { DetailsComponent } from './details/details.component';
+import { SharedModule } from '../shared/shared.module';
+
+
+
+@NgModule({
+  declarations: [HomeComponent, DetailsComponent],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    SharedModule
+  ]
+})
+export class PagesModule { }
